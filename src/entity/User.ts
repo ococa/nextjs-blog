47eef06e8e3590f4f3643dc523fa893e9c8bbfd1
ococa <wangchao.personal@gmail.com>
2021-03-09 22:04:09 +0800

    feat: typeorm init

---
+++ src/entity/User.ts
@@ -0,0 +1,18 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    age: number;
+
+}
