90e0a5caf6cd9f4db04bd29c3777a7e272cf5be8
ococa <wangchao.personal@gmail.com>
2021-03-11 08:54:58 +0800

    feat:: 统一命名风格

---
+++ dist/migration/1615423786288-RenameColumns.js
@@ -0,0 +1,103 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.RenameColumns1615423786288 = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var RenameColumns1615423786288 = /*#__PURE__*/function () {
+  function RenameColumns1615423786288() {
+    (0, _classCallCheck2["default"])(this, RenameColumns1615423786288);
+  }
+
+  (0, _createClass2["default"])(RenameColumns1615423786288, [{
+    key: "up",
+    value: function () {
+      var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return queryRunner.renameColumn('posts', 'author_id', 'authorId');
+
+              case 2:
+                _context.next = 4;
+                return queryRunner.renameColumn('comments', 'user_id', 'userId');
+
+              case 4:
+                _context.next = 6;
+                return queryRunner.renameColumn('comments', 'post_id', 'postId');
+
+              case 6:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function up(_x) {
+        return _up.apply(this, arguments);
+      }
+
+      return up;
+    }()
+  }, {
+    key: "down",
+    value: function () {
+      var _down = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(queryRunner) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.prev = 0;
+                _context2.next = 3;
+                return queryRunner.renameColumn('posts', 'authorId', 'author_id');
+
+              case 3:
+                _context2.next = 5;
+                return queryRunner.renameColumn('comments', 'userId', 'user_id');
+
+              case 5:
+                _context2.next = 7;
+                return queryRunner.renameColumn('comments', 'postId', 'post_id');
+
+              case 7:
+                _context2.next = 12;
+                break;
+
+              case 9:
+                _context2.prev = 9;
+                _context2.t0 = _context2["catch"](0);
+                console.error('e', _context2.t0);
+
+              case 12:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, null, [[0, 9]]);
+      }));
+
+      function down(_x2) {
+        return _down.apply(this, arguments);
+      }
+
+      return down;
+    }()
+  }]);
+  return RenameColumns1615423786288;
+}();
+
+exports.RenameColumns1615423786288 = RenameColumns1615423786288;
---
+++ src/migration/1615423786288-RenameColumns.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RenameColumns1615423786288 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.renameColumn('posts', 'author_id', 'authorId');
+        await queryRunner.renameColumn('comments', 'user_id', 'userId');
+        await queryRunner.renameColumn('comments', 'post_id', 'postId');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        try {
+            await queryRunner.renameColumn('posts','authorId', 'author_id');
+            await queryRunner.renameColumn('comments', 'userId', 'user_id');
+            await queryRunner.renameColumn('comments', 'postId', 'post_id');
+        } catch (e) {
+            console.error('e', e)
+        }
+    }
+}
