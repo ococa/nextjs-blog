082dd7b729cabf2080e691d5fb10f271244a1875
ococa <wangchao.personal@gmail.com>
2021-03-14 20:25:13 +0800

    feat: pagination

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,6 +2,7 @@ import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from '../../src/utils';
 import { Post } from "src/entity/Post";
 import Link from 'next/link';
+import { useEffect } from "react";
 
 type Props = {
   posts: {
@@ -10,34 +11,76 @@ type Props = {
     user: number,
     id: number,
   }[],
-  ctx
+  pagination: {
+    count: number,
+    pageSize: number,
+    pageNo: number
+  }
 }
 const PostIndex: NextPage<Props> = (props) => {
 
+  useEffect(() => {
+    // const
+    // const a = queryString(window.location.search);
+    // console.log(a)
+  }, [])
+
+
+  const { posts, pagination: { pageSize, pageNo, count} } = props;
+  console.log('page', {
+    pageSize,
+    pageNo
+  })
   return (
     <div>
       <h1>文章列表</h1>
       <hr/>
       {
-        props.posts.map(post => (<div>
+        posts.map(post => (<div key={post.id}>
           <Link href={`/posts/${post.id}`}>
             <a>{post.title}</a>
           </Link>
         </div>))
       }
+      <hr/>
+      <footer>
+        <div>
+          <Link
+            href={`/posts/?pageSize=${pageSize}&pageNo=${pageNo - 1}`}>
+            <a>上一页</a>
+          </Link>
+        </div>
+        <div>
+          <Link href={`/posts/?pageSize=${pageSize}&pageNo=${pageNo + 1}`}>
+            <a>下一页</a>
+          </Link>
+        </div>
+        总数量{count}
+      </footer>
     </div>
   )
 }
+
 
 export default PostIndex;
 
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  // const query = ctx.query;
+  let { pageSize = 10, pageNo = 1 } = ctx.query;
+  pageNo = parseInt(pageNo.toString()) > 0 ? parseInt(pageNo.toString()) : 1;
+  pageSize = parseInt(pageSize.toString());
   const connection = await getDatabaseConnection();
-  const a = await connection.manager.find(Post);
+  const a = await connection.manager.findAndCount(Post, { take: pageSize, skip:  pageSize * ( pageNo - 1) });
   return Promise.resolve({
     props: {
-      posts: JSON.parse(JSON.stringify(a)),
+      posts: JSON.parse(JSON.stringify(a[0])),
+      pagination: {
+        count: a[1],
+        pageSize,
+        pageNo,
+      }
     }
   })
 }
