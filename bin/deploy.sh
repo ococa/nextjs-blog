592d17c83fcb51f03d79635602df335050261121
ococa <wangchao.personal@gmail.com>
2021-03-15 21:59:42 +0800

    feat: update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,8 @@ git pull &&
 yarn install --production=false &&
 yarn build &&
 docker build -t wang/a . &&
+docker kill app &&
+docker rm app
 docker run --name app --network=host -p 3000:3000 -d wang/a &&
 echo "done"
 
