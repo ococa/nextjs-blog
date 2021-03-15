69be52f95481291badae9fb4560961cebd53527e
ococa <wangchao.personal@gmail.com>
2021-03-15 21:45:26 +0800

    Feat: update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,9 @@
+docker start d1a &&
+cd /home/root/nextjs-blog &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t wang/a . &&
+docker run --name app --network=host -p 3000:3000 -d wang/a &&
+echo "done"
 
-echo hello > /tmp/test
